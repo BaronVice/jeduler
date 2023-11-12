bce6d13559a8d460602d3b30b25c2fbe7a0e9d1d
BaronVice <toni.sas.228@mail.ru>
2023-11-12 23:31:30 +0800

    Minor additions + Statistics to implement

---
+++ src/main/java/com/bv/pet/jeduler/repositories/StatisticsRepository.java
@@ -0,0 +1,7 @@
+package com.bv.pet.jeduler.repositories;
+
+import com.bv.pet.jeduler.entities.Statistics;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatisticsRepository extends JpaRepository<Statistics, Long> {
+}
